40e197bcdc9b5797c6ff6e2dd79da56b2192f701
Chetan Mohite <cmohite@gmail.com>
2015-08-16 13:02:32 +1000

    quiz2 - question4 - commit

---
+++ getandclean_question4.R
@@ -0,0 +1,7 @@
+con <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlcode <- readLines(con)
+close(con)
+nchar(htmlcode[10])
+nchar(htmlcode[20])
+nchar(htmlcode[30])
+nchar(htmlcode[100])
